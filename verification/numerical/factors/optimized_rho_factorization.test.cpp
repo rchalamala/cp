64989dd85251c45db2478ca36571f18e1a334aff
rchalamala <rchalamala@outlook.com>
2021-03-25 17:49:28 -0600

    Update templates

---
+++ verification/numerical/factors/optimized_rho_factorization.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/factorize"
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+#include "../../../library/general/input.hpp"
+#include "../../../library/general/output.hpp"
+#include "../../../library/numerical/factors/optimized_rho_factorize.hpp"
+
+int main()
+{
+	std::cin.tie(nullptr)->std::ios_base::sync_with_stdio(false);
+	std::int_fast8_t q;
+	read(std::cin, q);
+	while(q--)
+	{
+		std::int_fast64_t a;
+		read(std::cin, a);
+		std::vector<std::int_fast64_t> factors = factors::optimized_rho_factorize<std::int_fast64_t>(a);
+		std::sort(std::begin(factors), std::end(factors));
+		prints(std::cout, std::size(factors), factors);
+	}
+	std::fflush(stdout);
+}
