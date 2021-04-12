469e0d79f65f29c1d06c5b92005f2468b0d42f65
rchalamala <rchalamala@outlook.com>
2021-04-12 14:19:14 -0600

    Update testing template to remove use of in and out, but std::cin and std::cout

--- library/general/input.hpp
+++ library/general/input.hpp
@@ -8,52 +8,52 @@
 #include <utility>
 #include <vector>
 
-template<typename Argument, typename... Arguments> bool read(std::istream& in, Argument& first, Arguments& ... rest);
+template<typename Argument, typename... Arguments> bool read(Argument& first, Arguments& ... rest);
 
-template<typename T, std::size_t N> bool read(std::istream& in, std::array<T, N>& elements);
+template<typename T, std::size_t N> bool read(std::array<T, N>& elements);
 
-template<typename T1, typename T2> bool read(std::istream& in, std::pair<T1, T2>& element);
+template<typename T1, typename T2> bool read(std::pair<T1, T2>& element);
 
-template<typename T> bool read(std::istream& in, T& element);
+template<typename T> bool read(T& element);
 
-template<typename T> bool read(std::istream& in, std::complex<T>& element);
+template<typename T> bool read(std::complex<T>& element);
 
-template<typename T> bool read(std::istream& in, std::vector<T>& elements);
+template<typename T> bool read(std::vector<T>& elements);
 
-template<typename Argument, typename... Arguments> bool read(std::istream& in, Argument& first, Arguments& ... rest)
+template<typename Argument, typename... Arguments> bool read(Argument& first, Arguments& ... rest)
 {
-	bool result = true;
-	result &= read(in, first);
-	result &= read(in, rest...);
+	bool result{true};
+	result &= read(first);
+	result &= read(rest...);
 	return result;
 }
 
-template<typename T, std::size_t Size> bool read(std::istream& in, std::array<T, Size>& elements)
+template<typename T, std::size_t Size> bool read(std::array<T, Size>& elements)
 {
-	bool result = true;
+	bool result{true};
 	for(T& element : elements)
-	{ result &= read(in, element); }
+	{ result &= read(element); }
 	return result;
 }
 
-template<typename T1, typename T2> bool read(std::istream& in, std::pair<T1, T2>& element) { return read(in, element.first, element.second); }
+template<typename T1, typename T2> bool read(std::pair<T1, T2>& element) { return read(element.first, element.second); }
 
-template<typename T> bool read(std::istream& in, T& element) { return static_cast<bool>(in >> element); }
+template<typename T> bool read(T& element) { return static_cast<bool>(std::cin >> element); }
 
-template<typename T> bool read(std::istream& in, std::complex<T>& element)
+template<typename T> bool read(std::complex<T>& element)
 {
-	bool result = true;
+	bool result{true};
 	T first, second;
-	result &= read(in, first, second);
+	result &= read(first, second);
 	element = std::complex<T>(first, second);
 	return result;
 }
 
-template<typename T> bool read(std::istream& in, std::vector<T>& elements)
+template<typename T> bool read(std::vector<T>& elements)
 {
-	bool result = true;
+	bool result{true};
 	for(T& element : elements)
-	{ result &= read(in, element); }
+	{ result &= read(element); }
 	return result;
 }
 
--- library/general/output.hpp
+++ library/general/output.hpp
@@ -4,44 +4,44 @@
 #include "to_string.hpp"
 #include <ostream>
 
-template<typename Argument, typename... Arguments> void print(std::ostream& out, const Argument& first, const Arguments& ... rest);
+template<typename Argument, typename... Arguments> void print(const Argument& first, const Arguments& ... rest);
 
-template<typename Argument, typename... Arguments> void printn(std::ostream& out, const Argument& first, const Arguments& ... rest);
+template<typename Argument, typename... Arguments> void printn(const Argument& first, const Arguments& ... rest);
 
-template<typename Argument, typename... Arguments> void prints(std::ostream& out, const Argument& first, const Arguments& ... rest);
+template<typename Argument, typename... Arguments> void prints(const Argument& first, const Arguments& ... rest);
 
-template<typename T> void print(std::ostream& out, const T& element);
+template<typename T> void print(const T& element);
 
-inline void printn(std::ostream& out);
+inline void printn();
 
-inline void prints(std::ostream& out);
+inline void prints();
 
-template<typename T> void print(std::ostream& out, const T& element) { out << std::to_string(element); }
+template<typename T> void print(const T& element) { std::cout << std::to_string(element); }
 
-inline void printn(std::ostream& out) { print(out, '\n'); }
+inline void printn() { print('\n'); }
 
-inline void prints(std::ostream& out) { print(out, '\n'); }
+inline void prints() { print('\n'); }
 
-template<typename Argument, typename... Arguments> void print(std::ostream& out, const Argument& first, const Arguments& ... rest)
+template<typename Argument, typename... Arguments> void print(const Argument& first, const Arguments& ... rest)
 {
-	print(out, first);
-	print(out, rest...);
+	print(first);
+	print(rest...);
 }
 
-template<typename Argument, typename... Arguments> void printn(std::ostream& out, const Argument& first, const Arguments& ... rest)
+template<typename Argument, typename... Arguments> void printn(const Argument& first, const Arguments& ... rest)
 {
-	print(out, first);
+	print(first);
 	if(sizeof...(rest))
-	{ prints(out); }
-	printn(out, rest...);
+	{ prints(); }
+	printn(rest...);
 }
 
-template<typename Argument, typename... Arguments> void prints(std::ostream& out, const Argument& first, const Arguments& ... rest)
+template<typename Argument, typename... Arguments> void prints(const Argument& first, const Arguments& ... rest)
 {
-	print(out, first);
+	print(first);
 	if(sizeof...(rest))
-	{ print(out, " "); }
-	prints(out, rest...);
+	{ print(" "); }
+	prints(rest...);
 }
 
 #endif
