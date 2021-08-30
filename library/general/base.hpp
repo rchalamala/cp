6bda2713d3cfafdffcc326a9a377f833b542eb4b
rchalamala <rchalamala@outlook.com>
2021-08-29 19:17:11 -0600

    random updates

--- library/general/base.hpp
+++ library/general/base.hpp
@@ -3,17 +3,8 @@
 
 #include <bits/stdc++.h>
 
-#define mp std::make_pair
-#define mt std::make_tuple
-#define pb push_back
-#define eb emplace_back
-#define ff first
-#define ss second
-
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
-
-
 
 #endif
